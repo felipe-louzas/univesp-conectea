fc971bacdc4ce4a549dae4df7f16a4e49c322d72
Felipe Louzas <felipe.louzas@gmail.com>
2024-05-25 17:57:27 -0300

    fix: click no logo deve voltar para home

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,13 +1,16 @@
 import { Box, Typography } from '@mui/material';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const Logo = () => {
     return (
         // Horizontal box
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Image src='/logo-appbar.png' alt='Logo' width={66} height={34} />
-            <Box sx={{ width: '0.8rem' }} />
-            <Typography variant='h3' color='white' component='span'>ConecTEA</Typography>
-        </Box>
+        <Link href='/'>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Image src='/logo-appbar.png' alt='Logo' width={66} height={34} />
+                <Box sx={{ width: '0.8rem' }} />
+                <Typography variant='h3' color='white' component='span'>ConecTEA</Typography>
+            </Box>
+        </Link>
     );
 }
